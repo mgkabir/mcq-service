bf81cd801a4cc9368bcdd76258db045cf5971002
Kabir M <mgkabir@gmail.com>
2017-01-10 00:46:28 +1100

    @CrossOrigin support added.

--- src/main/java/org/matrix/question/controller/QuestionController.java
+++ src/main/java/org/matrix/question/controller/QuestionController.java
@@ -12,7 +12,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 @RestController
 @RequestMapping(value = "/question")
 public class QuestionController {
