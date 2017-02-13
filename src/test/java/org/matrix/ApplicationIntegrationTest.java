1909d7d25d15dee177c0ce8fe975d81f4bb1756a
Kabir M <mgkabir@gmail.com>
2017-02-14 00:12:45 +1100

    Test for /practice-question implemented

---
+++ src/test/java/org/matrix/ApplicationIntegrationTest.java
@@ -0,0 +1,30 @@
+package org.matrix;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.matrix.question.model.Question;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class ApplicationIntegrationTest {
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@Test
+	public void test() {
+		ResponseEntity<Question> response = this.restTemplate.getForEntity("/practice-question", Question.class);
+		/* Both the statements below do the same thing */
+		Assertions.assertThat(response.getBody().getOptions().size()).isEqualTo(4);
+		Assert.assertEquals(4, response.getBody().getOptions().size());
+	}
+
+}
